09553f17f31fd9ba87cd5d6e61041065cec74490
Bart Van Bos <bart.vanbos@technicolor.com>
2013-08-01 17:26:53 +0200

    changed project structure

---
+++ java-lib/src/main/java/org/qeo/sms/rest/client/SmsRestClient.java
@@ -0,0 +1,23 @@
+package org.qeo.sms.rest.client;
+
+/************** COPYRIGHT AND CONFIDENTIALITY INFORMATION *********************
+ **                                                                          **
+ ** Copyright (c) 2013 Technicolor                                           **
+ ** All Rights Reserved                                                      **
+ **                                                                          **
+ ** This program contains proprietary information which is a trade           **
+ ** secret of TECHNICOLOR and/or its affiliates and also is protected as     **
+ ** an unpublished work under applicable Copyright laws. Recipient is        **
+ ** to retain this program in confidence and is not permitted to use or      **
+ ** make copies thereof other than as permitted in a written agreement       **
+ ** with TECHNICOLOR, UNLESS OTHERWISE EXPRESSLY ALLOWED BY APPLICABLE LAWS. **
+ **                                                                          **
+ ******************************************************************************/
+
+/**
+ * 
+ */
+public class SmsRestClient
+{
+
+}
