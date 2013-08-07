c21ce847489130d008afa5f1f6a3ab66a5576623
Bart Van Bos <bart.vanbos@technicolor.com>
2013-08-07 14:07:26 +0200

    Fixed checkstyle problems

---
+++ java-lib/src/main/java/org/qeo/sms/rest/interfaces/package-info.java
@@ -0,0 +1,19 @@
+/************** COPYRIGHT AND CONFIDENTIALITY INFORMATION *********************
+ **                                                                          **
+ ** Copyright (c) 2013 Technicolor                                           **
+ ** All Rights Reserved                                                      **
+ **                                                                          **
+ ** This program contains proprietary information which is a trade           **
+ ** secret of TECHNICOLOR and/or its affiliates and also is protected as     **
+ ** an unpublished work under applicable Copyright laws. Recipient is        **
+ ** to retain this program in confidence and is not permitted to use or      **
+ ** make copies thereof other than as permitted in a written agreement       **
+ ** with TECHNICOLOR, UNLESS OTHERWISE EXPRESSLY ALLOWED BY APPLICABLE LAWS. **
+ **                                                                          **
+ ******************************************************************************/
+
+/**
+ * Package containing the interfaces used by the SMS Client implementation.
+ */
+package org.qeo.sms.rest.interfaces;
+
